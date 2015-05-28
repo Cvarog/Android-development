1ccd08f5393dd21d2b0239a9ecc00400062a3a5a
Cvarog <uchihamilord@gmail.com>
2015-05-28 14:27:37 +0300

    Finished 2 lesson

---
+++ IntentTest/app/src/main/java/com/mycompany/intenttest/LastActivity.java
@@ -0,0 +1,10 @@
+package com.mycompany.intenttest;
+
+import android.app.Activity;
+
+/**
+ * Created by Артем on 06.05.2015.
+ */
+public class LastActivity extends Activity {
+
+}
--- IntentTest/app/src/main/java/com/mycompany/intenttest/MainActivity.java
+++ IntentTest/app/src/main/java/com/mycompany/intenttest/MainActivity.java
@@ -1,47 +1,31 @@
-package com.example.android.sunshine;
+package com.mycompany.intenttest;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
-                    .commit();
-        }
+    }
+
+    public void moveToNewActivity(View view){
+        Intent intent = new Intent(this, LastActivity.class);
+        startActivity(intent);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -59,5 +43,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
