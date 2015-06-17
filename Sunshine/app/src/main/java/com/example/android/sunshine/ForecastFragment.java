e47deba675ccc265f7542b3e7bcfd8bffc8fa6d7
Cvarog <uchihamilord@gmail.com>
2015-06-17 17:32:34 +0300

    Added Toast message when click on item

--- Sunshine/app/src/main/java/com/example/android/sunshine/ForecastFragment.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -39,6 +42,7 @@ import java.util.List;
 public class ForecastFragment extends Fragment {
 
     private ArrayAdapter<String> mForecastAdapter;
+    private static final String LOG_TAG = ForecastFragment.class.getSimpleName();
 
     public ForecastFragment() {
     }
@@ -100,6 +104,17 @@ public class ForecastFragment extends Fragment {
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+
+        listView.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int duration = Toast.LENGTH_SHORT;
+//                mForecastAdapter.getItem(position);
+//                Log.d(LOG_TAG, "Item is " + mForecastAdapter.getItem(position));
+                Toast toast = Toast.makeText(mForecastAdapter.getContext(),mForecastAdapter.getItem(position),duration);
+                toast.show();
+            }
+        });
 
         return rootView;
     }
